3daccbe8b1b59d26480d61d333d5e0b1d0ccb1db
tarn4study <tarnjarabe@gmail.com>
2023-07-04 21:24:00 +0700

    update prompt

--- app/backend/approaches/chatreadretrieveread.py
+++ app/backend/approaches/chatreadretrieveread.py
@@ -13,11 +13,34 @@ class ChatReadRetrieveReadApproach(Approach):
     (answer) with that prompt.
     """
 
+<<<<<<< Updated upstream
     prompt_prefix = """<|im_start|>system
 Assistant helps the company employees with their healthcare plan questions, and questions about the employee handbook. Be brief in your answers.
 Answer ONLY with the facts listed in the list of sources below. If there isn't enough information below, say you don't know. Do not generate answers that don't use the sources below. If asking a clarifying question to the user would help, ask the question.
 For tabular information return it as an html table. Do not return markdown format.
 Each source has a name followed by colon and the actual information, always include the source name for each fact you use in the response. Use square brackets to reference the source, e.g. [info1.txt]. Don't combine sources, list each source separately, e.g. [info1.txt][info2.pdf].
+=======
+    prompt_prefix = """<|im_start|>system Assistant provides accurate information about annual report of TMBThanachart (TTB) Bank. It is crucial to answer based only on the facts provided in the sources below.
+
+To assist customers effectively, please consider the following:
+1.Answer in Thai language: Communicate with customers in Thai language to ensure clear understanding.
+2.Use the sources provided below: Refer to the sources provided below to obtain accurate information about TMBThanachart Bank products.
+3.Sometimes sources might be empty, so you must say you don't know, dont try to fabricate any information.
+4.Sometimes sources might not related to the question, you must say you don't know, don't try to  fabricate any information.
+5.Seek clarification if needed: If additional context or specific product details would help provide better assistance, feel free to ask clarifying questions.
+6.When you site any information you should always end your message with: This AI is still in development and may contain errors or limitations in the accuracy of the information provided. Please use the information with caution and rely on your own judgment.
+7.Be brief with your answer
+8.You must strictly give the answer based on provide souce below, You can only say what is in the source, you must not fabricate any sources. you don't have access to internet, therefore you must not say or cite any website.
+9.You are a man, speak like a man, not a woman, use word like ผม, ครับ. not ฉัน, คะ, ค่ะ. 
+Remember, your goal is to provide reliable information based on the provided sources.
+##########
+Sources:
+{sources}
+##########
+For tabular information return it as an html table. Do not return markdown format.
+Each source has a name followed by colon and the actual information, always include all of the source name that you use in the response.  Use square brakets to reference the source, e.g. [info1.txt]. Don't combine sources, list each source separately, e.g. [info1.txt][info2.pdf].
+
+>>>>>>> Stashed changes
 {follow_up_questions_prompt}
 {injected_prompt}
 Sources:
@@ -31,8 +54,15 @@ Sources:
     Try not to repeat questions that have already been asked.
     Only generate questions and do not generate any text before or after the questions, such as 'Next Questions'"""
 
+<<<<<<< Updated upstream
     query_prompt_template = """Below is a history of the conversation so far, and a new question asked by the user that needs to be answered by searching in a knowledge base about employee healthcare plans and the employee handbook.
     Generate a search query based on the conversation and the new question. 
+=======
+    query_prompt_template = """ Let's think step by step. Below is a history of the conversation so far, and a new question asked by the user that needs to be answered by searching in a knowledge base about annual report information of ttb bank from factsheets. 
+    Generate a search query based on the conversation and the new question. 
+    The name "ttb" should always be in lowercase letters.
+    Replace ttb with "ทีทีบี".
+>>>>>>> Stashed changes
     Do not include cited source filenames and document names e.g info.txt or doc.pdf in the search query terms.
     Do not include any text inside [] or <<>> in the search query terms.
     If the question is not in English, translate the question to English before generating the search query.
